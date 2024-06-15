ef3d455a620c9200afcac0b9c7d69ff5637b5e4a
LuXts <1776137198@qq.com>
2024-06-15 20:27:45 +0800

    fix(download music): 修复歌曲名称里面包含不支持的符号时无法保存的问题

--- utils/storagex/cache.go
+++ utils/storagex/cache.go
@@ -62,8 +62,8 @@ func CopyCachedSong(song structs.Song) error {
 	musicType := split[len(split)-1]
 	filename := fmt.Sprintf("%s-%s.%s", song.Name, song.ArtistName(), musicType)
 	// Windows Linux 均不允许文件名中出现 / \ 替换为 _
-	filename = strings.ReplaceAll(filename, "/", "_")
-	filename = strings.ReplaceAll(filename, "\\", "_")
+	replacer := strings.NewReplacer("/", "_", "\\", "_", "*", "_", ":", "_", "\"", "_", "<", "_", ">", "_", "?", "_", "|", "_")
+	filename = replacer.Replace(filename)
 	targetFilename := filepath.Join(downloadDir, filename)
 
 	if _, err := os.Stat(targetFilename); err == nil {
